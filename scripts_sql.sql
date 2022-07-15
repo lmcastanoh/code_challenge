cec8f03f35ccea3f195bff336a5d41747829c089
Milena <milenestef@hotmail.com>
2022-07-15 12:27:27 -0500

    Final develpment code challenge

--- scripts_sql.sql
+++
@@ -1,9 +0,0 @@
-gcloud sql connect instancia-poc3-20220713 --user=root
-
-use code_challenge;
-CREATE TABLE hired_employees (id int, name char(100), datetime varchar(50), department_id int, job_id int);
-CREATE TABLE departments (id int,department char(100));
-CREATE TABLE jobs (id int,job char(100));
-
-
-
